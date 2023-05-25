00aea773a292994593477ee9ef154294c8f7b61c
antonioncoelho <52384521+antonioncoelho@users.noreply.github.com>
2023-05-25 13:14:30 +0100

    Now respects threads

--- javassist-tools/src/main/java/pt/ulisboa/tecnico/cnv/javassist/tools/ServerICount.java
+++ javassist-tools/src/main/java/pt/ulisboa/tecnico/cnv/javassist/tools/ServerICount.java
@@ -1,6 +1,8 @@
 package pt.ulisboa.tecnico.cnv.javassist.tools;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javassist.CannotCompileException;
 import javassist.CtBehavior;
@@ -22,25 +24,41 @@ public class ServerICount extends CodeDumper {
      */
     private static long ninsts = 0;
 
+    private static Map<Long, Long> saved_ninsts = new HashMap<Long, Long>();
+
     public ServerICount(List<String> packageNameList, String writeDestination) {
         super(packageNameList, writeDestination);
     }
 
-    public static void incBasicBlock(int position, int length) {
+    public static void incBasicBlock(int position, int length, long id) {
         nblocks++;
         ninsts += length;
+        long new_i;
+        if (saved_ninsts.get(id) == null) {
+            new_i = length;
+        } else {
+            new_i = saved_ninsts.get(id) + length;
+        }
+        //System.out.println("NEW VALUE OF INSTRUCTIONS " + new_i + "OF THREAD - " + id);
+        saved_ninsts.put(id, new_i);
     }
 
     public static void incBehavior(String name) {
         nmethods++;
     }
 
-    public static void resetVariables() {
+    public static void resetVariables(long id) {
         nblocks = ninsts = nmethods = 0;
+        System.out.println("RESETING INSTRUCTIONS OF THREAD - " + id);
+        saved_ninsts.put(id, 0L);
     }
 
     public static void inProcess() {
         System.out.println("JUST FINISHED PROCESSING IMAGE");
+    }
+
+    public static long getInstructions(long id) {
+        return saved_ninsts.get(id);
     }
 
     public static void printStatistics() {
@@ -56,16 +74,34 @@ public class ServerICount extends CodeDumper {
 
         if (behavior.getName().equals("main")) {
             behavior.insertAfter(String.format("%s.printStatistics();", ServerICount.class.getName()));
-            resetVariables();
+            //behavior.insertBefore(String.format("%s.resetVariables(Thread.currentThread().getId());", ServerICount.class.getName()));
         }
 
-        if (behavior.getName().equals("handle")) {
-            behavior.insertBefore(String.format("%s.resetVariables();", ServerICount.class.getName()));
+        if (behavior.getName().equals("runSimulation")) {
+            behavior.insertBefore(String.format("%s.resetVariables(Thread.currentThread().getId());", ServerICount.class.getName()));
             behavior.insertAfter(String.format("%s.printStatistics();", ServerICount.class.getName()));
+            //behavior.insertAfter(String.format("%s.saveInstructions();", ServerICount.class.getName()));
+            behavior.insertAfter(String.format("bruhInstructions(%s.getInstructions(Thread.currentThread().getId()));",  ServerICount.class.getName()));
         }
+
+        if (behavior.getName().equals("war")) {
+            behavior.insertBefore(String.format("%s.resetVariables(Thread.currentThread().getId());", ServerICount.class.getName()));
+            behavior.insertAfter(String.format("%s.printStatistics();", ServerICount.class.getName()));
+            //behavior.insertAfter(String.format("%s.saveInstructions();", ServerICount.class.getName()));
+            behavior.insertAfter(String.format("bruhInstructions(%s.getInstructions(Thread.currentThread().getId()));",  ServerICount.class.getName()));
+        }
+
+        if (behavior.getName().equals("process")) {
+            behavior.insertBefore(String.format("%s.resetVariables(Thread.currentThread().getId());", ServerICount.class.getName()));
+            behavior.insertAfter(String.format("%s.printStatistics();", ServerICount.class.getName()));
+            //behavior.insertAfter(String.format("%s.saveInstructions();", ServerICount.class.getName()));
+            behavior.insertAfter(String.format("bruhInstructions(%s.getInstructions(Thread.currentThread().getId()));",  ServerICount.class.getName()));
+        }
+
+        
 
         /* if (behavior.getName().equals("process")) {
-            behavior.insertBefore(String.format("%s.resetVariables();", ServerICount.class.getName()));
+            behavior.insertBefore(String.format("%s.resetVariables(Thread.currentThread().getId());", ServerICount.class.getName()));
             behavior.insertAfter(String.format("%s.inProcess();", ServerICount.class.getName()));
             behavior.insertAfter(String.format("%s.printStatistics();", ServerICount.class.getName()));
         } */
@@ -74,7 +110,7 @@ public class ServerICount extends CodeDumper {
     @Override
     protected void transform(BasicBlock block) throws CannotCompileException {
         super.transform(block);
-        block.behavior.insertAt(block.line, String.format("%s.incBasicBlock(%s, %s);", ServerICount.class.getName(), block.getPosition(), block.getLength()));
+        block.behavior.insertAfter(String.format("long idxxx = Thread.currentThread().getId();%s.incBasicBlock(%s, %s, idxxx);", ServerICount.class.getName(), block.getPosition(), block.getLength()));
     }
 
 }
