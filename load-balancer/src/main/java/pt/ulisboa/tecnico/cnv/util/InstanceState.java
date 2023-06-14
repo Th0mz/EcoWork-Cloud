3bdfb3ee3e7921ac4cade52d6e160792d00aa166
Tomás Tavares <57302012+Th0mz@users.noreply.github.com>
2023-06-14 15:15:50 +0100

    ok

--- load-balancer/src/main/java/pt/ulisboa/tecnico/cnv/util/InstanceState.java
+++ load-balancer/src/main/java/pt/ulisboa/tecnico/cnv/util/InstanceState.java
@@ -1,15 +1,41 @@
 package pt.ulisboa.tecnico.cnv.util;
 
 public class InstanceState {
+
+    private String id;
     private String url;
     private Double cpuAvg = null;
+    private long executingInstructions ;
 
-    public InstanceState(String url) {
+    public InstanceState(String id, String url) {
+        this.id = id;
         this.url = url;
+        this.executingInstructions = 0;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public void updateCPUAvg(Double newAvg) {
         this.cpuAvg = newAvg;
+    }
+
+    public long getExecutingInstructions() {
+        synchronized (this) {
+            return executingInstructions;
+        }
+    }
+    public void addInstructions(long instructions) {
+        synchronized (this) {
+            this.executingInstructions += instructions;
+        }
+    }
+
+    public void removeInstruction(long instructions) {
+        synchronized (this) {
+            this.executingInstructions -= instructions;
+        }
     }
 
     public String getUrl() {
